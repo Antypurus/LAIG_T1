9f8f62c7fe014fffb6a6fddbf0d2b8e102148f7f
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-10-01 14:04:38 +0100

    Removed all mentions of geometric transformation on primitives
    this comes after a design decision to move all transform to the object
    holding the primitives and no the primitive itself

--- LAIGDemoParser/LAIGDemoParser/DataStructures/Primitives/BasePrimitives/Triangle.js
+++ LAIGDemoParser/LAIGDemoParser/DataStructures/Primitives/BasePrimitives/Triangle.js
@@ -1,8 +1,6 @@
 function Triangle(CGFScene){
     this.scene = CGFScene
 
-    this.transforms = [];
-
     this.vertices = [
         0,0,0,
         2,0,0,
@@ -20,26 +18,7 @@ function Triangle(CGFScene){
 Triangle.prototype = Object.create(CGFobject.prototype);
 Triangle.prototype.constructor = Triangle;
 
-Triangle.prototype.setTransforms = function(transforms){
-    this.transforms = transforms;
-}
-
-Triangle.prototype.addTransform = function(transform){
-    this.transforms.push(transform);
-}
-
-Triangle.prototype.removeTransform = function(index){
-    this.transforms.splice(index,1);
-}
-
 Triangle.prototype.initBuffers = function(){
-    
-    this.scene.pushMatrix();
-    for(var i =0;i<this.transforms.length;i++){
-        this.transforms[i].act(this);//tentative
-    }
-    this.scene.popMatrix();
-
     this.primitiveType = this.scene.gl.TRIANGLES;
     this.initGLBuffers();
 }
--- LAIGDemoParser/LAIGDemoParser/DataStructures/Primitives/Primitive.js
+++ LAIGDemoParser/LAIGDemoParser/DataStructures/Primitives/Primitive.js
@@ -10,10 +10,9 @@ function primitive(type,vertices,id,scene){
   this.type = type;
   this.vertices = vertices;
   this.inidices = null;
-  this.transforms = null;
   this.scene = scene;
-  //this.setUP();
-  //this.initBuffers();
+  this.setUP();
+  this.initBuffers();
 }
 
 primitive.prototype = Object.create(CGFobject.prototype);
@@ -21,34 +20,14 @@ primitive.prototype.constructor = primitive;
 
 primitive.prototype.setUP = function(){
   for(var i = 0;i<this.vertices.length;++i){
-    
+
     if(i%2==0){
       this.inidices.push(i);
     }
   }
 }
 
-primitive.prototype.setTransforms = function(transforms){
-  this.transforms = transforms;
-  return;
-}
-
-primitive.prototype.addTransform = function(transform){
-  this.transforms.push(transform);
-  return;
-}
-
-primitive.prototype.removeTransform = function(index){
-  this.transforms.splice(index,1);
-  return;
-}
-
 primitive.prototype.initBuffers = function() {
-  this.pushMatrix();
-  for(var i=0;i<this.transforms.length();++i){
-    this.transforms[i].act(this);
-  }
-  this.popMatrix();
   this.initGLBuffers();
   return;
 }
