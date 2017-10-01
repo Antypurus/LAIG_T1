29010daca9771ffa53f8718f22d99a3c9f23a3a7
TiagoMagalhães(11506 <up201607931@fe.up.pt>
2017-10-01 22:30:26 +0100

    finished initial pass on the square primitive class

--- LAIGDemoParser/LAIGDemoParser/DataStructures/Primitives/BasePrimitives/Square.js
+++ LAIGDemoParser/LAIGDemoParser/DataStructures/Primitives/BasePrimitives/Square.js
@@ -3,7 +3,30 @@ function Square(Scene,Coord1,Coord2){
   this.coord1 = Coord1;
   this.coord2 = Coord2;
 
+  this.setUP();
+
   CGFobject.call(this,Scene);
+  this.initBuffers();
 }
 
 Square.prototype = Object.create(CGFobject.prototype);
+Square.prototype.constructor = Square;
+
+Square.prototype.setUP = function(){
+  this.vertives = [
+    this.coord1.x,this.coord1.y,0,
+    this.coord2.x,this.coord1.y,0,
+    this.coord2.x,this.coord2.y,0,
+    this.coord1.x,this.coord2.y,0
+  ];
+
+  this.indices = [
+    0,1,2,
+    0,2,3
+  ];
+}
+
+Square.prototype.initBuffers = function(){
+  this.primitiveType = this.scene.gl.TRIANGLES;
+  this.initGLBuffers();
+}
