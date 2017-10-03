0fd305b66ab85a6dc1356e85601bd03298b17f72
TiagoMagalhães(11506 <up201607931@fe.up.pt>
2017-10-03 11:20:44 +0100

    testing triangle

--- LAIGDemoParser/LAIGDemoParser/DataStructures/Primitives/BasePrimitives/Triangle.js
+++ LAIGDemoParser/LAIGDemoParser/DataStructures/Primitives/BasePrimitives/Triangle.js
@@ -15,29 +15,19 @@ Triangle.prototype.constructor = Triangle;
 
 Triangle.prototype.setUP = function(){
 
-  this.vertices_s = [
+  this.vertices = [
     this.coord1.x,this.coord1.y,this.coord1.z,
     this.coord2.x,this.coord2.y,this.coord2.z,
     this.coord3.x,this.coord3.y,this.coord3.z
   ];
 
-  this.indices_s = [
+  this.indices = [
     0,1,2,
   ];
 
 }
 
 Triangle.prototype.initBuffers = function(){
-    this.vertices = this.vertices_s;
-    this.indices = this.indices_s;
-
-    this.normals = [
-      0,0,1,
-      0,0,1,
-      0,0,1,
-      0,0,1
- 	];
-
     this.primitiveType = this.scene.gl.TRIANGLES;
     this.initGLBuffers();
 }
