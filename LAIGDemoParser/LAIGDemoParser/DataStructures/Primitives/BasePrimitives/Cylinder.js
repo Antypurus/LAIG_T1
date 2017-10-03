972ec016039963750553f260c868c5e4856d4388
TiagoMagalhães(11506 <up201607931@fe.up.pt>
2017-10-03 10:51:23 +0100

    finished basis for sphere and cylinder class

--- LAIGDemoParser/LAIGDemoParser/DataStructures/Primitives/BasePrimitives/Cylinder.js
+++ LAIGDemoParser/LAIGDemoParser/DataStructures/Primitives/BasePrimitives/Cylinder.js
@@ -1,6 +1,25 @@
-function Cylinder(Scene){
+function Cylinder(Scene,bottom_radius,top_radius,height,sections_along_height,parts_per_section){
+    this.scene = Scene;
+    this.bottom_radius = bottom_radius;
+    this.top_radius = top_radius;
+    this.height = height;
+    this.sections_along_height = sections_along_height;
+    this.parts_per_section = parts_per_section;
 
+    CGFobject.call(this,scene);
+
+    this.setUP();
+    this.initBuffers();
 }
 
 Cylinder.prototype = Object.create(CGFobject.prototype);
 Cylinder.prototype.constructor = Cylinder;
+
+Cylinder.prototype.setUP = function(){
+    //calc vertices and indices
+}
+
+Cylinder.prototype.initBuffers = function(){
+    this.primitiveType = this.scene.gl.TRIANGLES;
+    this.initGLBuffers();
+}
--- LAIGDemoParser/LAIGDemoParser/DataStructures/Primitives/BasePrimitives/Sphere.js
+++ LAIGDemoParser/LAIGDemoParser/DataStructures/Primitives/BasePrimitives/Sphere.js
@@ -1,6 +1,21 @@
-function Sphere(Scene){
+function Sphere(Scene,radiu,parts_along_radius,parts_per_section){
+    this.scene = Scene;
+    this.parts_along_radius = parts_along_radius;
+    this.parts_per_section = parts_per_section;
 
+    CGFobject.call(this.scene);
+    this.setUP();
+    this.initBuffers();
 }
 
 Sphere.prototype = Object.create(CGFobject.prototype);
 Sphere.prototype.constructor = Sphere;
+
+Sphere.prototype.setUP = function(){
+    //calc vertices and indices
+}
+
+Sphere.prototype.initBuffers = function(){
+    this.primitiveType = this.scene.gl.TRIANGLES;
+    this.initGLBuffers();
+}
