38fa860e5e2d72281acb6da1da2f2c9b5fc61352
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-10-15 15:55:43 +0100

    properly implemented cilinder to vary in radius

--- LAIGDemoParser/LAIGDemoParser/DataStructures/Primitives/BasePrimitives/Cylinder.js
+++ LAIGDemoParser/LAIGDemoParser/DataStructures/Primitives/BasePrimitives/Cylinder.js
@@ -1,73 +1,89 @@
-function Cylinder(Scene,height,bottom_radius,top_radius,sections_along_height,parts_per_section){
-    this.scene = Scene;
-    this.bottom_radius = bottom_radius;
-    this.top_radius = top_radius;
-    this.height = height;
-    this.slices = sections_along_height;
-    this.stacks = parts_per_section;
-    this.circle = new Circle(Scene, this.stacks, bottom_radius);
+function Cylinder(Scene, height, bottom_radius, top_radius, sections_along_height, parts_per_section) {
+	this.scene = Scene;
+	this.bottom_radius = bottom_radius;
+	this.top_radius = top_radius;
+	this.height = height;
+	this.slices = sections_along_height;
+	this.stacks = parts_per_section;
+	this.circle = new Circle(Scene, this.stacks, 1);
 
-    CGFobject.call(this,Scene);
+	CGFobject.call(this, Scene);
 
-    this.setUP();
-    this.initBuffers();
+	this.setUP();
+	this.initBuffers();
 }
 
 Cylinder.prototype = Object.create(CGFobject.prototype);
 Cylinder.prototype.constructor = Cylinder;
 
-Cylinder.prototype.setUP = function(){
-    
-	var alpha =(2*Math.PI)/this.slices;
-	var beta = alpha/2;
+Cylinder.prototype.setUP = function () {
+
+	var deltaR=this.top_radius-this.bottom_radius;
+	var dr=deltaR/this.stacks;
+
+	var alpha = (2 * Math.PI) / this.slices;
+	var beta = alpha / 2;
 	var ind = 0;
-	this.vertices =[];
+	this.vertices = [];
 	this.indices = [];
 	this.normals = [];
 	this.texCoords = [];
-	for(var z = 0; z < this.stacks; z++){
-	var sli = z/this.stacks;
-	var sliplus1 = sli+1/this.stacks;
-		
-	for(var i = 0; i < this.slices; i++){
-	this.vertices.push(Math.cos(i*alpha)*this.bottom_radius,Math.sin(i*alpha)*this.bottom_radius,sli * this.height);
-	this.vertices.push(Math.cos((i+1)*alpha)*this.bottom_radius, Math.sin((i+1)*alpha)*this.bottom_radius,sli * this.height);
-	this.vertices.push(Math.cos((i+1)*alpha)*this.bottom_radius, Math.sin((i+1)*alpha)*this.bottom_radius,sliplus1 * this.height);
-	this.vertices.push(Math.cos(i*alpha)*this.bottom_radius, Math.sin(i*alpha)*this.bottom_radius,sliplus1 * this.height);
 
-	this.indices.push(ind,ind +1 ,ind + 3);
-	this.indices.push(ind+1,ind+2,ind+3);
-	this.indices.push(ind+3,ind +1 ,ind);
-	this.indices.push(ind+3,ind+2,ind+1);
+	var topRad=0;
+	var botRad=0;
 
-	this.normals.push(Math.cos(i*alpha),Math.sin(i*alpha),sli);
-	this.normals.push(Math.cos((i+1)*alpha), Math.sin((i+1)*alpha),sli);
-	this.normals.push(Math.cos((i+1)*alpha), Math.sin((i+1)*alpha),sliplus1);
-	this.normals.push(Math.cos(i*alpha), Math.sin(i*alpha),sliplus1);
- 	ind += 4;
-	
-	this.texCoords.push(1 - i / this.slices, z / this.stacks);
-	this.texCoords.push(1 - (i + 1) / this.slices, z / this.stacks);		
-	this.texCoords.push(1 - i / this.slices, (z + 1) / this.stacks);
-	this.texCoords.push(1 - (i + 1) / this.slices, (z + 1) / this.stacks);
-	}
+	for (var z = 0; z < this.stacks; z++) {
+		var sli = z / this.stacks;
+		var sliplus1 = sli + 1 / this.stacks;
+
+		if(z==0){
+			botRad=this.bottom_radius;
+			topRad=botRad+dr;
+		}else{
+			botRad+=dr;
+			topRad=botRad+dr;
+		}
+
+		for (var i = 0; i < this.slices; i++) {
+			this.vertices.push(Math.cos(i * alpha) * botRad, Math.sin(i * alpha) * botRad, sli * this.height);
+			this.vertices.push(Math.cos((i + 1) * alpha) * botRad, Math.sin((i + 1) * alpha) * botRad, sli * this.height);
+			this.vertices.push(Math.cos((i + 1) * alpha) * topRad, Math.sin((i + 1) * alpha) * topRad, sliplus1 * this.height);
+			this.vertices.push(Math.cos(i * alpha) * topRad, Math.sin(i * alpha) * topRad, sliplus1 * this.height);
+
+			this.indices.push(ind, ind + 1, ind + 3);
+			this.indices.push(ind + 1, ind + 2, ind + 3);
+			this.indices.push(ind + 3, ind + 1, ind);
+			this.indices.push(ind + 3, ind + 2, ind + 1);
+
+			this.normals.push(Math.cos(i * alpha), Math.sin(i * alpha), sli);
+			this.normals.push(Math.cos((i + 1) * alpha), Math.sin((i + 1) * alpha), sli);
+			this.normals.push(Math.cos((i + 1) * alpha), Math.sin((i + 1) * alpha), sliplus1);
+			this.normals.push(Math.cos(i * alpha), Math.sin(i * alpha), sliplus1);
+			ind += 4;
+
+			this.texCoords.push(1 - i / this.slices, z / this.stacks);
+			this.texCoords.push(1 - (i + 1) / this.slices, z / this.stacks);
+			this.texCoords.push(1 - i / this.slices, (z + 1) / this.stacks);
+			this.texCoords.push(1 - (i + 1) / this.slices, (z + 1) / this.stacks);
+		}
 	}
 
 }
 
-Cylinder.prototype.initBuffers = function(){
-    this.primitiveType = this.scene.gl.TRIANGLES;
-    this.initGLBuffers();
+Cylinder.prototype.initBuffers = function () {
+	this.primitiveType = this.scene.gl.TRIANGLES;
+	this.initGLBuffers();
 }
 
-Cylinder.prototype.display = function()
-{
+Cylinder.prototype.display = function () {
 	this.scene.pushMatrix();
+	this.scene.scale(this.bottom_radius,this.bottom_radius,1);
 	this.circle.display();
 	this.scene.popMatrix();
 
 	this.scene.pushMatrix();
-	this.scene.translate(0,0,this.height);
+	this.scene.scale(this.top_radius,this.top_radius,1);
+	this.scene.translate(0, 0, this.height);
 	this.circle.display();
 	this.scene.popMatrix();
 
