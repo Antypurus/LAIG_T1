1902d1be46a9d0d752f78e6d70950da82983107a
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-11-15 21:10:22 +0000

    fixed the linear animation

--- LAIGDemoParser/ArtifactScene/Animations/LinearAnimation.js
+++ LAIGDemoParser/ArtifactScene/Animations/LinearAnimation.js
@@ -30,29 +30,36 @@ function LinearAnimation(velocity, controlPoints) {
 LinearAnimation.prototype = Object.create(CGFobject.prototype);
 LinearAnimation.prototype.constructor = LinearAnimation;
 
-LinearAnimation.prototype.setUp = function() {
+LinearAnimation.prototype.setUp = function () {
   for (let i = 0; i < this.controlPoints.length - 1; ++i) {
     let vec = [];
     vec.push(this.controlPoints[i + 1][0] - this.controlPoints[i][0]);
     vec.push(this.controlPoints[i + 1][1] - this.controlPoints[i][1]);
     vec.push(this.controlPoints[i + 1][2] - this.controlPoints[i][2]);
+
+    console.log("DELTA VEC:" + vec);
+
     this.azimuth.push(Math.atan2(vec[0], vec[2]));
     let ac = Math.sqrt(
       Math.pow(this.controlPoints[i + 1][0] - this.controlPoints[i][0], 2) +
-        Math.pow(this.controlPoints[i + 1][2] - this.controlPoints[i][2], 2)
+      Math.pow(this.controlPoints[i + 1][2] - this.controlPoints[i][2], 2)
     );
     this.elevation.push(Math.atan2(vec[1], ac));
-    this.vx.push(
-      this.velocity * Math.cos(this.azimuth[i]) * Math.cos(this.elevation[i])
-    );
-    this.vy.push(
-      this.velocity * Math.sin(this.azimuth[i]) * Math.cos(this.elevation[i])
-    );
-    this.vz.push(this.velocity * Math.sin(this.elevation[i]));
+    //this.vx.push(
+    //this.velocity * Math.cos(this.azimuth[i]) * Math.cos(this.elevation[i])
+    //);
+    //this.vy.push(
+    //this.velocity * Math.sin(this.azimuth[i]) * Math.cos(this.elevation[i])
+    //);
+    //this.vz.push(this.velocity * Math.sin(this.elevation[i]));
+
+    this.vx.push(vec[0] / this.velocity);
+    this.vy.push(vec[1] / this.velocity);
+    this.vz.push(vec[2] / this.velocity);
   }
 };
 
-LinearAnimation.prototype.update = function(currTime) {
+LinearAnimation.prototype.update = function (currTime) {
   var timeElapsed = 0;
 
   timeElapsed = currTime / 1000 - this.prevTime;
@@ -64,10 +71,10 @@ LinearAnimation.prototype.update = function(currTime) {
       this.deltaY += timeElapsed * this.vy[this.currStep];
       this.deltaZ += timeElapsed * this.vz[this.currStep];
     }
-    //console.log("time elapsed:" + timeElapsed);
-    //console.log("dx:" + this.deltaX + " vx:" + this.vx[this.currStep]);
-    //console.log("dy:" + this.deltaY + " vy:" + this.vy[this.currStep]);
-    //console.log("dz:" + this.deltaZ + " vz:" + this.vz[this.currStep]);
+    console.log("time elapsed:" + timeElapsed);
+    console.log("dx:" + this.deltaX + " vx:" + this.vx[this.currStep]);
+    console.log("dy:" + this.deltaY + " vy:" + this.vy[this.currStep]);
+    console.log("dz:" + this.deltaZ + " vz:" + this.vz[this.currStep]);
 
     mat4.identity(this.matrix);
     mat4.translate(this.matrix, this.matrix, [
@@ -78,30 +85,70 @@ LinearAnimation.prototype.update = function(currTime) {
 
     let vec = [];
     vec.push(
-      this.controlPoints[this.currStep + 1][0] -
-        this.controlPoints[this.currStep][0]
+      this.controlPoints[this.currStep + 1][0]
     );
     vec.push(
-      this.controlPoints[this.currStep + 1][1] -
-        this.controlPoints[this.currStep][1]
+      this.controlPoints[this.currStep + 1][1]
     );
     vec.push(
-      this.controlPoints[this.currStep + 1][2] -
-        this.controlPoints[this.currStep][2]
+      this.controlPoints[this.currStep + 1][2]
     );
 
-    if (
-      this.deltaX >= vec[0] &&
-      this.deltaY >= vec[1] &&
-      this.deltaZ >= vec[2]
-    ) {
+    let f1 = false;
+    let f2 = false;
+    let f3 = false;
+
+    if(vec[0]>=0){
+      if(this.deltaX>=vec[0]){
+        f1=true;
+      }else{
+        f1=false;
+      }
+    }else{
+      if(this.deltaX<=vec[0]){
+        f1=true;
+      }else{
+        f1=false;
+      }
+    }
+
+    if(vec[1]>=0){
+      if(this.deltaY>=vec[1]){
+        f2=true;
+      }else{
+        f2=false;
+      }
+    }else{
+      if(this.deltaY<=vec[1]){
+        f2=true;
+      }else{
+        f2=false;
+      }
+    }
+
+    if(vec[2]>=0){
+      if(this.deltaZ>=vec[2]){
+        f3=true;
+      }else{
+        f3=false;
+      }
+    }else{
+      if(this.deltaZ<=vec[2]){
+        f2=true;
+      }else{
+        f2=false;
+      }
+    }
+
+    if (f1 && f2 && f3) {
       this.currStep++;
+      this.timeElapsed = 0;
     }
 
     this.prevTime = currTime / 1000;
   }
 };
 
-LinearAnimation.prototype.applyAnimation = function() {
+LinearAnimation.prototype.applyAnimation = function () {
   return this.matrix;
 };
