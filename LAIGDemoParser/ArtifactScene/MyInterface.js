62197de90c185a58a56b65847b143aed751afd2b
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-11-25 23:35:57 +0000

    changed color picker name to fit interface size

--- LAIGDemoParser/ArtifactScene/MyInterface.js
+++ LAIGDemoParser/ArtifactScene/MyInterface.js
@@ -25,7 +25,7 @@ MyInterface.prototype.init = function(application) {
     this.gui = new dat.GUI();
 
     // add a oup of controls (and open/expand by defult)
-    this.gui.addColor(this.scene, 'filterColor').name("Selected Node Filter Color");
+    this.gui.addColor(this.scene, 'filterColor').name("Filter Color");
     
     return true;
 };
