1159daa209e3016d981b88d02c449e24a1d19b55
Tiago José Sousa Magalhães <up201607931@fe.up.pt>
2017-12-24 22:18:10 +0000

    more work on the gameboard logic

--- LAIGDemoParser/ArtifactScene/GameEntities/GameBoard.js
+++ LAIGDemoParser/ArtifactScene/GameEntities/GameBoard.js
@@ -15,4 +15,28 @@ function GameBoard(scene, startX, startY, startZ, nCells, sCell) {
   this.startZ = startZ;
   this.nCells = nCells;
   this.sCell = sCell;
+
+  this.coordinates = {};
+  this.idsToRegister = {};
+  this.idCoordMap = new Map();
+}
+
+/**
+ * This Function sets up the different data structure required to manage the
+ * game board
+ */
+GameBoard.prototype.setUp = function() {
+  for (let i = 0; i < this.nCells; ++i) {
+    for (let j = i; j < this.nCells; ++j) {
+      // add the upper values
+      this.coordinates.push({i, j});
+      this.idsToRegister.push('' + i + j);
+      this.idCoordMap['' + i + j] = {i, j};
+      // add the lower values
+      this.coordinates.push({j, i});
+      this.idsToRegister.push('' + j + i);
+      this.idCoordMap['' + j + i] = {j, i};
+    }
+  }
+  console.log(this.idCoordMap);
 }
