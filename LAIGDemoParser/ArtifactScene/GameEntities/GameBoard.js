637470c8eb03f9322424498e11a45cd041b7c6cb
Tiago José Sousa Magalhães <up201607931@fe.up.pt>
2017-12-24 22:04:57 +0000

    doc

--- LAIGDemoParser/ArtifactScene/GameEntities/GameBoard.js
+++ LAIGDemoParser/ArtifactScene/GameEntities/GameBoard.js
@@ -2,11 +2,11 @@
  *Class that handles game board logic
  *
  * @param {CGFscene} scene the scene
- * @param {float} startX the start x coordinate of the board
- * @param {float} startY the start y coordinate of the board
- * @param {float} startZ the start z coordinate of the board
- * @param {int} nCells the number of cells in one direction of the board
- * @param {float} sCell the size of each cell of the board
+ * @param {number} startX the start x coordinate of the board
+ * @param {number} startY the start y coordinate of the board
+ * @param {number} startZ the start z coordinate of the board
+ * @param {number} nCells the number of cells in one direction of the board
+ * @param {number} sCell the size of each cell of the board
  */
 function GameBoard(scene, startX, startY, startZ, nCells, sCell) {
   this.scene = scene;
