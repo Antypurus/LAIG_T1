442c2d33a03330f52c452bd9d36d1d0d9020fe67
Tiago José Sousa Magalhães <up201607931@fe.up.pt>
2018-01-01 18:53:45 +0000

    changes

--- LAIGDemoParser/ArtifactScene/GameEntities/GamePiece.js
+++ LAIGDemoParser/ArtifactScene/GameEntities/GamePiece.js
@@ -36,31 +36,31 @@ function GamePiece(scene, color, position) {
 
   switch (this.Color) {
     case ('green'): {
-      this.mat.setAmbient(0, 1, 0, 1);
+      this.mat.setAmbient(0, 0.1, 0, 1);
       this.mat.setDiffuse(0, 0.2, 0, 1);
       this.mat.setSpecular(0, 0.7, 0, 1);
       break;
     }
     case ('blue'): {
-      this.mat.setAmbient(0, 0, 1, 1);
+      this.mat.setAmbient(0, 0, 0.1, 1);
       this.mat.setDiffuse(0, 0, 0.2, 1);
       this.mat.setSpecular(0, 0, 0.7, 1);
       break;
     }
     case ('yellow'): {
-      this.mat.setAmbient(1, 1, 0, 1);
+      this.mat.setAmbient(0.1, 0.1, 0, 1);
       this.mat.setDiffuse(0.2, 0.2, 0, 1);
       this.mat.setSpecular(0.7, 0.7, 0, 1);
       break;
     }
     case ('red'): {
-      this.mat.setAmbient(1, 0, 0, 1);
+      this.mat.setAmbient(0.1, 0, 0, 1);
       this.mat.setDiffuse(0.2, 0, 0, 1);
       this.mat.setSpecular(0.7, 0, 0, 1);
       break;
     }
     default: {
-      this.mat.setAmbient(1, 1, 1, 1);
+      this.mat.setAmbient(0.1, 0.1, 0.1, 1);
       this.mat.setDiffuse(0.2, 0.2, 0.2, 1);
       this.mat.setSpecular(0.7, 0.7, 0.7, 1);
       break;
@@ -75,33 +75,33 @@ function GamePiece(scene, color, position) {
 GamePiece.prototype.setUpColor = function(color) {
   switch (color) {
     case ('green'): {
-      this.mat.setAmbient(0, 1, 0, 1);
-      this.mat.setDiffuse(0, 1, 0, 1);
-      this.mat.setSpecular(0, 1, 0, 1);
+      this.mat.setAmbient(0, 0.1, 0, 1);
+      this.mat.setDiffuse(0, 0.2, 0, 1);
+      this.mat.setSpecular(0, 0.7, 0, 1);
       break;
     }
     case ('blue'): {
-      this.mat.setAmbient(0, 0, 1, 1);
-      this.mat.setDiffuse(0, 0, 1, 1);
-      this.mat.setSpecular(0, 0, 1, 1);
+      this.mat.setAmbient(0, 0, 0.1, 1);
+      this.mat.setDiffuse(0, 0, 0.2, 1);
+      this.mat.setSpecular(0, 0, 0.7, 1);
       break;
     }
     case ('yellow'): {
-      this.mat.setAmbient(1, 1, 0, 1);
-      this.mat.setDiffuse(1, 1, 0, 1);
-      this.mat.setSpecular(1, 1, 0, 1);
+      this.mat.setAmbient(0.1, 0.1, 0, 1);
+      this.mat.setDiffuse(0.2, 0.2, 0, 1);
+      this.mat.setSpecular(0.7, 0.7, 0, 1);
       break;
     }
     case ('red'): {
-      this.mat.setAmbient(1, 0, 0, 1);
-      this.mat.setDiffuse(1, 0, 0, 1);
-      this.mat.setSpecular(1, 0, 0, 1);
+      this.mat.setAmbient(0.1, 0, 0, 1);
+      this.mat.setDiffuse(0.2, 0, 0, 1);
+      this.mat.setSpecular(0.7, 0, 0, 1);
       break;
     }
     default: {
-      this.mat.setAmbient(1, 1, 1, 1);
-      this.mat.setDiffuse(1, 1, 1, 1);
-      this.mat.setSpecular(1, 1, 1, 1);
+      this.mat.setAmbient(0.1, 0.1, 0.1, 1);
+      this.mat.setDiffuse(0.2, 0.2, 0.2, 1);
+      this.mat.setSpecular(0.7, 0.7, 0.7, 1);
       break;
     }
   }
